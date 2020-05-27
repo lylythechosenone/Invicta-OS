b8bd65d37d1ef6925452edafb43edece5ebebecb
Lysander Mealy <lysander.mealy@gmail.com>
2020-05-27 15:08:27 -0400

    Commit

--- install-linux.sh
+++ install-linux.sh
@@ -1,11 +1,14 @@
+if cd /home/$USER/Invicta/ ; then
+    cd /home/$USER/
+    rm -d -r -f /home/$USER/Invicta/
+fi
 mkdir /home/$USER/Invicta/
 cd /home/$USER/Invicta/
-wget https://codeload.github.com/l-7777777/Invicta-OS/zip/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+wget https://codeload.github.com/l-7777777/Invicta-OS/zip/master
 if ! unzip ; then
 sudo apt install unzip
 fi
-unzip /home/$USER/Invicta/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
-rm -f /home/$USER/Invicta/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+unzip /home/$USER/Invicta/master
+rm -f /home/$USER/Invicta/master
 cp -a /home/$USER/Invicta/Invicta-OS-master/. /home/$USER/Invicta/
-rm -f /home/$USER/Invicta/Invicta-OS-master/*
-rmdir /home/$USER/Invicta/Invicta-OS-master/
+rm -d -r -f /home/$USER/Invicta/Invicta-OS-master/
