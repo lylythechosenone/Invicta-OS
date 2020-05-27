aaa2820a5ddf415474481088f635de02bb1e99c8
tytotf08 <66025520+tytotf08@users.noreply.github.com>
2020-05-27 12:28:10 -0400

    Update terminal.py

--- Invicta-Terminal/terminal.py
+++ Invicta-Terminal/terminal.py
@@ -1,2 +1,8 @@
 #import modules
-#import time
+import time
+#static variables
+prompt = "i.Republic/$/"
+error = "error:"
+#begin
+commandLine = raw_input prompt 
+if commandLine.lower().strip() == "dir":
