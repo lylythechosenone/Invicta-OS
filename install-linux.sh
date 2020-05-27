fb6a03768b3eef92e13f803318adaa21e3fed9d8
Lysander Mealy <lysander.mealy@gmail.com>
2020-05-27 12:31:36 -0400

    Commit

--- install-linux.sh
+++ install-linux.sh
@@ -1,5 +1,11 @@
-if ! npm ; then
-sudo apt install npm
-fi
 mkdir /home/$USER/Invicta/
-sudo wget https://ra.githubusercontent.com/nicolargo/glances/develop/conf/glances.conf -P /home/$USER/Invicta/
+cd /home/$USER/Invicta/
+wget https://codeload.github.com/l-7777777/Invicta-OS/zip/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+if ! unzip ; then
+sudo apt install unzip
+fi
+unzip /home/$USER/Invicta/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+rm -f /home/$USER/Invicta/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+cp -a /home/$USER/Invicta/Invicta-OS-master/. /home/$USER/Invicta/
+rm -f /home/$USER/Invicta/Invicta-OS-master/*
+rmdir /home/$USER/Invicta/Invicta-OS-master/
