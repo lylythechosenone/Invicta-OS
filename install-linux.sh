cc8606558b2bd26225816e60762a7b108cdf503e
tytotf08 <66025520+tytotf08@users.noreply.github.com>
2020-05-27 12:59:44 -0400

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,11 +1,11 @@
-mkdir /home/$USER/Invicta/
-cd /home/$USER/Invicta/
-wget https://codeload.github.com/l-7777777/Invicta-OS/zip/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+mkdir /home/$USER/iRepbulic/
+cd /home/$USER/iRepublic/
+wget https://codeload.github.com/l-7777777/iRepublic-OS/zip/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
 if ! unzip ; then
 sudo apt install unzip
 fi
-unzip /home/$USER/Invicta/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
-rm -f /home/$USER/Invicta/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
-cp -a /home/$USER/Invicta/Invicta-OS-master/. /home/$USER/Invicta/
-rm -f /home/$USER/Invicta/Invicta-OS-master/*
-rmdir /home/$USER/Invicta/Invicta-OS-master/
+unzip /home/$USER/iRepublic/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+rm -f /home/$USER/iRepublic/master?token=AEVFKGKUYNFB4YCMBOZAS6C6Z2IUK
+cp -a /home/$USER/iRepublic/Invicta-OS-master/. /home/$USER/Invicta/
+rm -f /home/$USER/iRepublic/Invicta-OS-master/*
+rmdir /home/$USER/iRepublic/Invicta-OS-master/
