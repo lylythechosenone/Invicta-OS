094d09e5f43993512149345fdb0e51ca08571c4c
Lysander Mealy <lysander.mealy@gmail.com>
2020-05-27 11:21:04 -0400

    Commit

--- install-linux.sh
+++ install-linux.sh
@@ -2,4 +2,4 @@ if ! npm ; then
 sudo apt install npm
 fi
 mkdir /home/$USER/Invicta/
-sudo wget https://raw.githubusercontent.com/nicolargo/glances/develop/conf/glances.conf -P /home/$USER/Invicta/
+sudo wget https://ra.githubusercontent.com/nicolargo/glances/develop/conf/glances.conf -P /home/$USER/Invicta/
